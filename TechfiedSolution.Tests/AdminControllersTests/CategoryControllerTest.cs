529d21679f1a8b55a573f598e1398544c7012d91
Michael Shepherd M <shepherd.munemo@outlook.com>
2023-09-29 11:02:24 +0200

    AdminCategoryController.Tests

--- TechfiedSolution.Tests/AdminControllersTests/CategoryControllerTest.cs
+++ TechfiedSolution.Tests/AdminControllersTests/CategoryControllerTest.cs
@@ -19,11 +19,11 @@ public class CategoryControllerTest
             new Category { Id = 2, Title = "Category 2", ThumbnailImagePath = "default.jpg" }
         };
 
-        var dbContextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+        var dbContextMock = new DbContextOptionsBuilder<ApplicationDbContext>()
             .UseInMemoryDatabase(databaseName: "TestDatabase")
             .Options;
 
-        using var dbContext = new ApplicationDbContext(dbContextOptions);
+        using var dbContext = new ApplicationDbContext(dbContextMock);
         dbContext.AddRange(categories);
         dbContext.SaveChanges();
 
@@ -37,4 +37,68 @@ public class CategoryControllerTest
         var model = Assert.IsAssignableFrom<List<Category>>(viewResult.Model);
         Assert.Equal(2, model.Count); // Check that there are 2 categories in the model
     }
+
+    [Fact]
+    public async Task Details_IdIsNull_ReturnsNotFound()
+    {
+        // Arrange
+        var dbContextMock = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: "TestDatabase")
+            .Options;
+
+        using var dbContext = new ApplicationDbContext(dbContextMock);
+        var controller = new CategoryController(dbContext);
+
+        // Act
+        var result = await controller.Details(null);
+
+        // Assert
+        Assert.IsType<NotFoundResult>(result);
+    }
+
+    [Fact]
+    public async Task Create_ValidModel_ReturnsRedirectToActionResult()
+    {
+        // Arrange
+        var dbContextMock = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: "TestDatabase")
+            .Options;
+
+        using var dbContext = new ApplicationDbContext(dbContextMock);
+        var controller = new CategoryController(dbContext);
+        var category = new Category { Id = 2, Title = "Category 2", ThumbnailImagePath = "default.jpg" };
+        // Act
+        var result = await controller.Create(category);
+
+        // Assert
+        Assert.IsType<RedirectToActionResult>(result);
+    }
+
+    [Fact]
+    public async Task DeleteConfirmed_ValidId_ReturnsRedirectToActionResult()
+    {
+        // Arrange
+        var dbContextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: "TestDatabase")
+            .Options;
+
+        using var dbContext = new ApplicationDbContext(dbContextOptions);
+        dbContext.Category.Add(new Category
+        {
+            Id = 1,
+            Title = "Test Title",
+            ThumbnailImagePath = "Test.jpg"
+        }); // a sample category with ID 1 to the in-memory database
+        dbContext.SaveChanges();
+
+        var controller = new CategoryController(dbContext);
+
+        // Act
+        var result = await controller.DeleteConfirmed(1);
+
+        // Assert
+        Assert.IsType<RedirectToActionResult>(result);
+    }
+
+
 }
--- TechfiedSolution/Areas/Admin/Controllers/CategoryController.cs
+++ TechfiedSolution/Areas/Admin/Controllers/CategoryController.cs
@@ -50,8 +50,6 @@ public class CategoryController : Controller
     }
 
     // POST: Admin/Category/Create
-    // To protect from overposting attacks, enable the specific properties you want to bind to.
-    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
     [HttpPost]
     [ValidateAntiForgeryToken]
     public async Task<IActionResult> Create([Bind("Id,Title,Description,ThumbnailImagePath")] Category category)
